bbcacea6e9218acd52869d0dbfe3a9849297db2c
Greg Shiner <greg@gregshiner.com>
2025-12-24 13:46:56 -0500

    very atomic commit

--- query_engine/src/external.rs
+++ query_engine/src/external.rs
@@ -7,68 +7,82 @@ pub enum DataType {
 
 #[repr(C)]
 struct ExternalTableView {
-    columns: *mut ExternalColumnView,
+    columns: *const ExternalColumnView,
     num_of_columns: usize,
 }
 
 #[repr(C)]
 struct ExternalColumnView {
-    name: *mut u8,
+    name: *const u8,
     name_len: usize,
-    chunks: *mut ExternalChunkView,
+    chunks: *const ExternalChunkView,
     num_of_chunks: usize,
     data_type: DataType,
 }
 
 #[repr(C)]
 struct ExternalChunkView {
-    sub_chunks: *mut *mut u8,
-    referenced_chunks: *mut usize,
-    sub_chunk_lens: *mut usize,
+    sub_chunks: *const ExternalSubChunkView,
+    referenced_chunks: *const usize,
+    sub_chunk_lens: *const usize,
     num_of_sub_chunks: usize,
+}
+
+#[repr(C)]
+struct ExternalSubChunkView {
+    data: *const u8,
+    offsets: *const usize,
+    lengths: *const usize,
+    num_of_items: usize,
 }
 
 struct TableView<'a> {
     columns: &'a [ColumnView<'a>],
 }
 
-pub enum ColumnView<'a> {
-    Int(IntColumn<'a>),
-    Float(FloatColumn<'a>),
-    String(StringColumn<'a>),
+pub enum ChunkViews<'a> {
+    Int(&'a [IntChunk<'a>]),
+    Float(&'a [FloatChunk<'a>]),
+    String(&'a [StringChunk<'a>]),
 }
-
-pub struct IntColumn<'a> {
+pub struct ColumnView<'a> {
     pub name: &'a str,
-    pub chunks: &'a [IntChunk<'a>],
+    pub chunks: ChunkViews<'a>,
 }
 
 pub struct IntChunk<'a> {
-    pub values: &'a [i32],
-}
-
-pub struct FloatColumn<'a> {
-    pub name: &'a str,
-    pub chunks: &'a [FloatChunk<'a>],
+    pub sub_chunks: &'a [IntSubChunk<'a>],
 }
 
 pub struct FloatChunk<'a> {
-    pub values: &'a [f32],
-}
-
-pub struct StringColumn<'a> {
-    pub name: &'a str,
-    pub chunks: &'a [StringChunk<'a>],
+    pub sub_chunks: &'a [FloatSubChunk<'a>],
 }
 
 pub struct StringChunk<'a> {
-    pub values: &'a str,
+    pub sub_chunks: &'a [StringSubChunk<'a>],
 }
 
-impl From<ExternalChunkView> for StringChunk {
-    unsafe fn from(value: ExternalChunkView) -> Self {
-        IntChunk {
-            values: core::slice::from_raw_parts(data, len),
+pub struct IntSubChunk<'a> {
+    pub values: &'a [i32],
+}
+
+pub struct FloatSubChunk<'a> {
+    pub values: &'a [f32],
+}
+
+pub struct StringSubChunk<'a> {
+    pub values: Vec<&'a str>,
+}
+
+impl<'a> IntSubChunk<'a> {
+    unsafe fn from_external(sub_chunk: ExternalSubChunkView) -> IntSubChunk<'a> {
+        unsafe {
+            IntSubChunk {
+                values: core::slice::from_raw_parts(
+                    sub_chunk.data as *const i32,
+                    sub_chunk.num_of_items,
+                ),
+            }
         }
     }
 }
