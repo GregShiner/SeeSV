8878b4576e2fea69cbffcf806dbb209d5feb53f9
Greg Shiner <greg@gregshiner.com>
2025-12-27 00:36:21 -0500

    genericify this shit

--- query_engine/src/external.rs
+++ query_engine/src/external.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use core::slice;
+
 #[repr(C)]
 pub enum ExternalDataType {
     Int,
@@ -47,139 +49,88 @@ pub struct ColumnView<'a> {
 }
 
 pub enum ChunkViews<'a> {
-    Int(Vec<IntChunk<'a>>),
-    Float(Vec<FloatChunk<'a>>),
-    String(Vec<StringChunk<'a>>),
+    Int(Vec<Chunk<'a, IntValues<'a>>>),
+    Float(Vec<Chunk<'a, FloatValues<'a>>>),
+    String(Vec<Chunk<'a, StringValues<'a>>>),
 }
 
-pub struct IntChunk<'a> {
-    pub sub_chunks: Vec<IntSubChunk<'a>>,
+pub struct Chunk<'a, T> {
+    pub sub_chunks: Vec<SubChunk<'a, T>>,
 }
 
-pub struct FloatChunk<'a> {
-    pub sub_chunks: Vec<FloatSubChunk<'a>>,
+pub struct SubChunk<'a, T> {
+    pub values: T,
+    _phantom: std::marker::PhantomData<&'a ()>,
 }
 
-pub struct StringChunk<'a> {
-    pub sub_chunks: Vec<StringSubChunk<'a>>,
-}
+pub type IntValues<'a> = &'a [i32];
+pub type FloatValues<'a> = &'a [f32];
+pub type StringValues<'a> = Vec<&'a str>;
 
-pub struct IntSubChunk<'a> {
-    pub values: &'a [i32],
-}
-
-pub struct FloatSubChunk<'a> {
-    pub values: &'a [f32],
-}
-
-pub struct StringSubChunk<'a> {
-    pub values: Vec<&'a str>,
-}
-
-trait FromExternalSubChunk<'a> {
+trait FromExternalSubChunk<'a>: Sized {
     unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> Self;
-}
-
-impl<'a> FromExternalSubChunk<'a> for IntSubChunk<'a> {
-    /// # Safety
-    /// - `sub_chunk.data` must be valid for `'a`
-    /// - `sub_chunk.data` must be a valid array of aligned i32s
-    /// - `num_of_items` must match number of f32's in array
-    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> IntSubChunk<'a> {
-        unsafe {
-            IntSubChunk {
-                values: core::slice::from_raw_parts(
-                    sub_chunk.data as *const i32,
-                    sub_chunk.num_of_items,
-                ),
-            }
-        }
-    }
-}
-
-impl<'a> FromExternalSubChunk<'a> for FloatSubChunk<'a> {
-    /// # Safety
-    /// - `sub_chunk.data` must be valid for `'a`
-    /// - `sub_chunk.data` must be a valid array of aligned f32s
-    /// - `num_of_items` must match number of f32's in array
-    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> FloatSubChunk<'a> {
-        unsafe {
-            FloatSubChunk {
-                values: core::slice::from_raw_parts(
-                    sub_chunk.data as *const f32,
-                    sub_chunk.num_of_items,
-                ),
-            }
-        }
-    }
-}
-
-impl<'a> FromExternalSubChunk<'a> for StringSubChunk<'a> {
-    /// # Safety
-    /// - `sub_chunk.data`, `offsets`, and `lengths` must be valid for `'a`
-    /// - Offsets and lengths must describe valid UTF-8 slices within `data`
-    /// - `num_of_items` must match offsets/lengths arrays
-    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> StringSubChunk<'a> {
-        let mut values = Vec::with_capacity(sub_chunk.num_of_items);
-        unsafe {
-            let offsets = core::slice::from_raw_parts(sub_chunk.offsets, sub_chunk.num_of_items);
-            let lengths = core::slice::from_raw_parts(sub_chunk.lengths, sub_chunk.num_of_items);
-            for i in 0..sub_chunk.num_of_items {
-                values.push(str::from_utf8_unchecked(core::slice::from_raw_parts(
-                    sub_chunk.data.wrapping_add(offsets[i]),
-                    lengths[i],
-                )));
-            }
-        }
-        StringSubChunk { values }
-    }
 }
 
 trait FromExternalChunk<'a> {
     unsafe fn from_external(chunk: &'a ExternalChunkView) -> Self;
 }
 
-impl<'a> FromExternalChunk<'a> for IntChunk<'a> {
-    unsafe fn from_external(chunk: &'a ExternalChunkView) -> IntChunk<'a> {
-        // let mut sub_chunks = Vec::with_capacity(chunk.num_of_sub_chunks)
+impl<'a> FromExternalSubChunk<'a> for SubChunk<'a, IntValues<'a>> {
+    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> Self {
         unsafe {
-            let external_sub_chunks =
-                core::slice::from_raw_parts(chunk.sub_chunks, chunk.num_of_sub_chunks);
-            let sub_chunks: Vec<_> = external_sub_chunks
-                .iter()
-                .map(|external_sub_chunk| IntSubChunk::from_external(external_sub_chunk))
-                .collect();
-            IntChunk { sub_chunks }
+            SubChunk {
+                values: slice::from_raw_parts(sub_chunk.data as *const i32, sub_chunk.num_of_items),
+                _phantom: std::marker::PhantomData,
+            }
         }
     }
 }
 
-impl<'a> FromExternalChunk<'a> for FloatChunk<'a> {
-    unsafe fn from_external(chunk: &'a ExternalChunkView) -> FloatChunk<'a> {
-        // let mut sub_chunks = Vec::with_capacity(chunk.num_of_sub_chunks)
+impl<'a> FromExternalSubChunk<'a> for SubChunk<'a, FloatValues<'a>> {
+    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> Self {
         unsafe {
-            let external_sub_chunks =
-                core::slice::from_raw_parts(chunk.sub_chunks, chunk.num_of_sub_chunks);
-            let sub_chunks: Vec<_> = external_sub_chunks
-                .iter()
-                .map(|external_sub_chunk| FloatSubChunk::from_external(external_sub_chunk))
-                .collect();
-            FloatChunk { sub_chunks }
+            SubChunk {
+                values: slice::from_raw_parts(sub_chunk.data as *const f32, sub_chunk.num_of_items),
+                _phantom: std::marker::PhantomData,
+            }
         }
     }
 }
 
-impl<'a> FromExternalChunk<'a> for StringChunk<'a> {
-    unsafe fn from_external(chunk: &'a ExternalChunkView) -> StringChunk<'a> {
+impl<'a> FromExternalSubChunk<'a> for SubChunk<'a, StringValues<'a>> {
+    unsafe fn from_external(sub_chunk: &'a ExternalSubChunkView) -> Self {
+        let mut values = Vec::with_capacity(sub_chunk.num_of_items);
+        unsafe {
+            let offsets = slice::from_raw_parts(sub_chunk.offsets, sub_chunk.num_of_items);
+            let lengths = slice::from_raw_parts(sub_chunk.lengths, sub_chunk.num_of_items);
+            for i in 0..sub_chunk.num_of_items {
+                values.push(str::from_utf8_unchecked(slice::from_raw_parts(
+                    sub_chunk.data.wrapping_add(offsets[i]),
+                    lengths[i],
+                )));
+            }
+        }
+        SubChunk {
+            values,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, T> FromExternalChunk<'a> for Chunk<'a, T>
+where
+    SubChunk<'a, T>: FromExternalSubChunk<'a>,
+{
+    unsafe fn from_external(chunk: &'a ExternalChunkView) -> Chunk<'a, T> {
         // let mut sub_chunks = Vec::with_capacity(chunk.num_of_sub_chunks)
         unsafe {
             let external_sub_chunks =
-                core::slice::from_raw_parts(chunk.sub_chunks, chunk.num_of_sub_chunks);
+                slice::from_raw_parts(chunk.sub_chunks, chunk.num_of_sub_chunks);
             let sub_chunks: Vec<_> = external_sub_chunks
                 .iter()
-                .map(|external_sub_chunk| StringSubChunk::from_external(external_sub_chunk))
+                .map(|external_sub_chunk| SubChunk::from_external(external_sub_chunk))
                 .collect();
-            StringChunk { sub_chunks }
+            Chunk { sub_chunks }
         }
     }
 }
@@ -188,29 +139,34 @@ impl<'a> ColumnView<'a> {
     unsafe fn from_external(column: &'a ExternalColumnView) -> ColumnView<'a> {
         unsafe {
             let name =
-                str::from_utf8_unchecked(core::slice::from_raw_parts(column.name, column.name_len));
-            let external_chunks = core::slice::from_raw_parts(column.chunks, column.num_of_chunks);
+                str::from_utf8_unchecked(slice::from_raw_parts(column.name, column.name_len));
+            let external_chunks = slice::from_raw_parts(column.chunks, column.num_of_chunks);
+
+            #[rustfmt::skip]
             let chunks = match column.data_type {
                 ExternalDataType::Int => ChunkViews::Int(
-                    external_chunks
-                        .iter()
-                        .map(|external_chunk| IntChunk::from_external(external_chunk))
-                        .collect(),
+                    Self::convert_chunks(external_chunks)
                 ),
                 ExternalDataType::Float => ChunkViews::Float(
-                    external_chunks
-                        .iter()
-                        .map(|external_chunk| FloatChunk::from_external(external_chunk))
-                        .collect(),
+                    Self::convert_chunks(external_chunks)
                 ),
                 ExternalDataType::String => ChunkViews::String(
-                    external_chunks
-                        .iter()
-                        .map(|external_chunk| StringChunk::from_external(external_chunk))
-                        .collect(),
+                    Self::convert_chunks(external_chunks)
                 ),
             };
             ColumnView { name, chunks }
+        }
+    }
+
+    unsafe fn convert_chunks<T>(external_chunks: &'a [ExternalChunkView]) -> Vec<Chunk<'a, T>>
+    where
+        Chunk<'a, T>: FromExternalChunk<'a>,
+    {
+        unsafe {
+            external_chunks
+                .iter()
+                .map(|external_chunk| Chunk::from_external(external_chunk))
+                .collect()
         }
     }
 }
@@ -218,7 +174,7 @@ impl<'a> ColumnView<'a> {
 impl<'a> TableView<'a> {
     unsafe fn from_external(table: &'a ExternalTableView) -> TableView<'a> {
         unsafe {
-            let external_columns = core::slice::from_raw_parts(table.columns, table.num_of_columns);
+            let external_columns = slice::from_raw_parts(table.columns, table.num_of_columns);
             TableView {
                 columns: external_columns
                     .iter()
