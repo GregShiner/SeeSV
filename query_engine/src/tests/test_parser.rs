d18601e0c9706e00b44485940cab5e6147050fb7
Greg Shiner <greg@gregshiner.com>
2025-12-29 22:38:22 -0500

    Fix tests

--- query_engine/src/tests/test_parser.rs
+++ query_engine/src/tests/test_parser.rs
@@ -3,28 +3,12 @@ mod tests {
     use crate::parser::grammar;
     use rstest::rstest;
 
-    #[test]
-    fn test_query_parser() {
-        use crate::ast::*;
-
-        let parser = grammar::QueryParser::new();
-
-        assert_eq!(
-            parser.parse("SELECT * FROM test;").unwrap(),
-            Query::Select(SelectQuery {
-                select_exprs: vec![SelectExpr::All],
-                table_ref: Some(TableRef::Identifier(Identifier::from("test")))
-            })
-        )
-    }
-
-    #[rstest]
-    #[case("SELECT * FROM users;")]
-    #[case("select * from users;")]
-    #[case("SELECT  *  FROM  users;")]
-    fn test_valid_queries(#[case] input: &str) {
-        let parser = grammar::SelectQueryParser::new();
-        assert!(parser.parse(input).is_ok());
+    macro_rules! set_snapshot_suffix {
+        ($($expr:expr),*) => {
+            let mut settings = insta::Settings::clone_current();
+            settings.set_snapshot_suffix(format!($($expr,)*));
+            let _guard = settings.bind_to_scope();
+        }
     }
 
     #[rstest]
@@ -32,22 +16,25 @@ mod tests {
     #[case("SELECT FROM users;")]
     #[case("SELECT*FROMusers;")]
     #[case("* FROM users;")]
+    #[case("SELECT * FROM table")]
     fn test_invalid_queries(#[case] input: &str) {
         let parser = grammar::SelectQueryParser::new();
         assert!(parser.parse(input).is_err());
     }
 
     #[rstest]
-    #[case("SELECT * FROM users;")]
-    #[case("SELECT column FROM users;")]
-    #[case(r#"SELECT column FROM "test";"#)]
-    #[case(r#"SELECT "column";"#)]
-    #[case(r#"SELECT 5;"#)]
-    #[case(r#"SELECT 5.5;"#)]
-    #[case(r#"SELECT .5;"#)]
-    fn test_select_query_ast(#[case] input: &str) {
-        let parser = grammar::SelectQueryParser::new();
+    #[case(1, "SELECT * FROM users;")]
+    #[case(2, "SELECT column FROM users;")]
+    #[case(3, r#"SELECT column FROM "test";"#)]
+    #[case(4, r#"SELECT "column";"#)]
+    #[case(5, r#"SELECT 5;"#)]
+    #[case(6, r#"SELECT 5.5;"#)]
+    #[case(7, r#"SELECT .5;"#)]
+    fn test_ast(#[case] id: i32, #[case] input: &str) {
+        let parser = grammar::QueryParser::new();
         let query = parser.parse(input).unwrap();
+
+        set_snapshot_suffix!("{}", id);
 
         // Put the input and query in the snapshot to make it easier to see which query goes to
         // which output
