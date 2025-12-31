b438777bf62843db217d0a7160546bbc1ad9f43e
Greg Shiner <greg@gregshiner.com>
2025-12-31 01:16:42 -0500

    Add planner output to main

--- query_engine/src/main.rs
+++ query_engine/src/main.rs
@@ -4,8 +4,13 @@ mod parser;
 mod planner;
 mod tests;
 
+use planner::ExecutionPlan;
+
 use crate::parser::grammar;
-use std::io::{self, Read};
+use std::{
+    collections::HashSet,
+    io::{self, Read},
+};
 
 fn main() {
     // Read input from stdin
@@ -18,13 +23,34 @@ fn main() {
     // Assuming your parser module is named 'parser' and has a parser called 'ProgramParser'
     let parser = grammar::QueryParser::new();
 
-    match parser.parse(&input) {
-        Ok(cst) => {
-            println!("{:#?}", cst); // Pretty-print the debug output
+    let cst = parser.parse(&input);
+    match cst {
+        Ok(ref cst) => {
+            println!(
+                "Parse Tree for {}",
+                &input.strip_suffix("\n").unwrap_or(&input)
+            );
+            println!("{cst:#?}"); // Pretty-print the debug output
         }
         Err(e) => {
-            eprintln!("Parse error: {:?}", e);
+            eprintln!("Parse error: {e:?}");
             std::process::exit(1);
+        }
+    };
+    let cst = cst.unwrap();
+    let plan: ExecutionPlan = cst.into();
+    println!("Execution Plan:");
+    println!("{plan:#?}");
+    println!("Operations executed in this order:");
+    let mut completed = HashSet::new();
+    loop {
+        let ready_ops = plan.get_ready_nodes(&completed);
+        for op_id in &ready_ops {
+            println!("{}: {:?}", op_id, plan.nodes.get(op_id));
+            completed.insert(*op_id);
+        }
+        if ready_ops.is_empty() {
+            break;
         }
     }
 }
